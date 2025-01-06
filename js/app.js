8e3900e850d56d337076df3f4fdd03a995f292cb
Alex Javier <alexavier1989@gmail.com>
2025-01-06 14:16:18 -0500

    feat: Guess the number game completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,71 @@
 "use strict";
+
+const guessInput = document.getElementById("guess-input");
+const resultMessage = document.getElementById("guess-message");
+const currentGuess = document.getElementById("current-guess");
+const computerGuess = document.getElementById("computer-guess");
+const guessHistory = document.getElementById("guess-history");
+
+const submitButton = document.getElementById("submit-btn");
+const restartButton = document.getElementById("restart-btn");
+
+let numberToGuess = Math.floor(Math.random() * 10) + 1;
+let attemptsHistory = [];
+let attemps = 3;
+
+const endGame = (codeValue) => {
+  // show animation by codeValue
+  computerGuess.textContent = numberToGuess;
+  submitButton.disabled = true;
+  restartButton.disabled = false;
+}
+
+const renderMessages = (result, userGuess) => {
+    resultMessage.textContent = result;
+    currentGuess.textContent = userGuess;
+    attemptsHistory.push(userGuess);
+    guessHistory.textContent = attemptsHistory.join(", ");
+
+}
+
+const checkGuess = () => {
+    const guessNumber = parseInt(guessInput.value);
+    if( isNaN(guessNumber) || guessNumber < 1 || guessNumber > 10 ) {
+        resultMessage.textContent = "Please enter a valid number between 1 and 10";
+        return;
+    }
+    if(guessNumber === numberToGuess) {
+        renderMessages("Congratulations! You guessed the number!", guessNumber);
+        endGame(0);
+    } else {
+        attemps--;
+        if(attemps === 0) {
+            renderMessages("Sorry! You lost, try again!", guessNumber);
+            endGame(1);
+        } else {
+            const message = guessNumber < numberToGuess ? "Nope! Try a higher number" : "Nope! Try a lower number";
+            renderMessages(message, guessNumber); 
+        }
+    }
+
+}
+
+const restartGame = () => {
+    // reset values
+    numberToGuess = Math.floor(Math.random() * 10) + 1;
+    attemptsHistory = [];
+    attemps = 3;
+    // reset UI
+    guessInput.value = "";
+    resultMessage.textContent = "";
+    currentGuess.textContent = "";
+    computerGuess.textContent = "";
+    guessHistory.textContent = "";
+    submitButton.disabled = false;
+    restartButton.disabled = true;
+}
+
+
+submitButton.addEventListener("click", checkGuess);
+
+restartButton.addEventListener("click", restartGame);
